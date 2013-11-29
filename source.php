19bafa6dd4f166b78e8969d79c6ae7fd0b28e7ae
Danny Yan <yan4646@hotmail.com>
2013-11-28 19:59:08 -0500

    Included Utilities and ConnectToDatabase file

--- source.php
+++ source.php
@@ -1,35 +1,35 @@
 <?php
-require_once('../include/ConnectToDatabase.php');
-header('Content-Type: text/event-stream');
-header('Cache-Control: no-cache'); // recommended to prevent caching of event data.
+	require_once(__DIR__.'/ConnectToDatabase.php');
+	header('Content-Type: text/event-stream');
+	header('Cache-Control: no-cache'); // recommended to prevent caching of event data.
 
-function sendMsg($msg) {
-	echo "data: $msg" . PHP_EOL;
-	echo PHP_EOL;
+	function sendMsg($msg) {
+		echo "data: $msg" . PHP_EOL;
+		echo PHP_EOL;
+		ob_flush();
+		flush();
+	}
+
+	function CountRows($statement) {
+		if (empty($statement)) {
+			return 0;
+		}
+		$result = mysql_query($statement);
+		$row_count = mysql_fetch_row($result);
+		return $row_count[0];
+	}
+
+	echo PHP_EOL; // need to pre flush eol;
 	ob_flush();
 	flush();
-}
-
-function CountRows($statement) {
-	if (empty($statement)) {
-		return 0;
+	$statement = 'SELECT COUNT(*) FROM test';
+	$old_count = CountRows($statement);
+	while(true) {
+		$new_count = CountRows($statement);
+		if($old_count != $new_count) {
+			$old_count = $new_count;
+			sendMsg($old_count);
+		}
+		sleep(1);
 	}
-	$result = mysql_query($statement);
-	$row_count = mysql_fetch_row($result);
-	return $row_count[0];
-}
-
-echo PHP_EOL; // need to pre flush eol;
-ob_flush();
-flush();
-$statement = 'SELECT COUNT(*) FROM test';
-$old_count = CountRows($statement);
-while(true) {
-	$new_count = CountRows($statement);
-	if($old_count != $new_count) {
-		$old_count = $new_count;
-		sendMsg($old_count);
-	}
-	sleep(1);
-}
 ?>
