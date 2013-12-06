69d53db7efe0dbfbd84cb533424b61f39b87d7f3
Danny Yan <yan4646@hotmail.com>
2013-12-06 00:16:19 -0500

    Added Google Api Font

---
+++ ActiveUsersManager.php
@@ -0,0 +1,20 @@
+<?php 
+	require_once(__DIR__.'/ConnectToDatabase.php');
+
+	function updateActive($user, $active) {
+		$statementUpdateActive = 'UPDATE active_users SET active='.$active.' WHERE user="'.$user.'"';
+		$resultUpdateActive = mysql_query($statementUpdateActive);
+	}
+
+	if(!empty($_POST['query'])) {
+		if($_POST['query'] == 'UpdateUserActive') {
+			if(!empty($_POST['user']) && !empty($_POST['active'])) {
+				updateActive($_POST['user'], $_POST['active']);
+				echo 'notempty';
+			} else {
+				echo 'empty';
+			}
+		}
+	}
+
+?>
