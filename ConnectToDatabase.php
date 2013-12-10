387e582108c5eb0be962788bde5b48e05de91ad2
Danny Yan <yan4646@hotmail.com>
2013-12-09 23:54:08 -0500

    Removed dead reference

--- ConnectToDatabase.php
+++ ConnectToDatabase.php
@@ -1,10 +1,15 @@
 <?php 
 
-$link = mysql_connect('localhost','root',''); 
+$host = 'localhost';
+$user = 'root';
+$pass = '';
+$database = 'database';
+
+$link = mysql_connect($host, $user, $pass); 
 if (!$link) { 
 	die('Could not connect to MySQL: ' . mysql_error()); 
 } 
-$db_selected = mysql_select_db('database', $link);
+$db_selected = mysql_select_db($database, $link);
 if (!$db_selected) {
     die ('Can\'t use database : ' . mysql_error());
 }
